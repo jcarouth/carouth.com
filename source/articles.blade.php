53567dfbe9ad90dcc1837aa45deb73e4abc8c18a
Jeff Carouth <jcarouth@gmail.com>
2021-03-07 17:49:42 -0600

    Update articles list page layout

--- source/articles.blade.php
+++ source/articles.blade.php
@@ -16,26 +16,23 @@ $page->template = 'content_hub';
 
 @section('body')
     <div class="max-w-6xl mx-auto px-3 md:px-10 pt-4 pb-12 bg-ghost-white" >
-        <h1 class="mt-8 mb-12">Articles</h1>
+        <h1 class="mt-8">Articles</h1>
 
-        <div class="lg:w-5/6 space-y-12">
+        <div class="lg:w-5/6 mt-8 divide-y-2 divide-y-gray-100">
             @foreach ($pagination->items as $post)
-            <div class="p-6 bg-gray-100 rounded-lg shadow-md" data-ga-element="post_card">
-                <h2 class="text-3xl mt-2 leading-tight">
+            <div class="py-6" data-ga-element="post_card">
+                <time datetime="{{ $post->getDate()->format('Y-m-d') }}" class="mt-0 font-serif text-gray-700 text-sm">
+                    {{ $post->getDate()->format('F j, Y') }}
+                </time>
+                <h2 class="mt-2 leading-tight">
                     <a class="no-underline" href="{{ $post->getUrl() }}" title="Read {{ $post->title }}" data-ga-click data-ga-item="title">
                         {{ $post->title }}
                     </a>
                 </h2>
 
-                <p class="mt-6">{!! $post->getExcerpt() !!}</p>
+                <p class="max-w-3xl mt-6 mb-0">{!! $post->getExcerpt() !!}</p>
 
-                <a class="block mt-4 w-40 py-3 px-6 bg-dark-cerulean text-center text-ghost-white font-semibold uppercase tracking-wide rounded-lg no-underline"
-                    href="{{ $post->getUrl() }}"
-                    title="Read - {{ $post->title }}"
-                    data-ga-click data-ga-item="button"
-                >
-                    Read Post
-                </a>
+                <a href="{{ $post->getUrl() }}" title="Read more - {{ $post->title }}" class="block mt-6 uppercase font-semibold tracking-wide no-underline">Read Post</a>
             </div>
             @endforeach
         </div>
